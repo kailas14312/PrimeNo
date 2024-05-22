5c80eca0e6d204844a86f778cc8701f5f947191a
Adesh <mamtadahiwal@gmail.com>
2024-05-22 14:41:55 +0530

    prime no code added

---
+++ Siddhi Jewellers/src/PrimeNoSerise.java
@@ -0,0 +1,22 @@
+public class PrimeNoSerise {
+	
+	public static void main(String[] args) {
+		
+		int no =20;
+		int counter=0;
+		
+		for(int i=2; i<=no; i++) {
+			for(int j=2; j<=i-1; j++) {
+				
+				if(i%j==0) {
+					counter++;
+					}
+			}if(counter==0) {
+				
+				System.out.println(i);
+			}else counter = 0;
+		}
+	}
+
+}
+
