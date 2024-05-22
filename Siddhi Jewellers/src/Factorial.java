0a1a77097a215240b473c6f7d03f519714acb031
Adesh <mamtadahiwal@gmail.com>
2024-05-22 16:44:11 +0530

    factorial added

---
+++ Siddhi Jewellers/src/Factorial.java
@@ -0,0 +1,15 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+        int totalfact=0;
+		int no = 5;
+		int fact = 1;
+		for (int i = fact; i <= no; i++) {
+			fact = fact * i;
+			System.out.println(fact);
+			
+			totalfact=totalfact+fact;
+		}System.out.println(totalfact);
+    }
+}
--- Siddhi Jewellers/src/fibonacci.java
+++ Siddhi Jewellers/src/fibonacci.java
@@ -1,19 +1,18 @@
 
-
 public class fibonacci {
 
 	public static void main(String[] args) {
-		int no=10;
-		int a=0;
-		int b=1;
-		System.out.println(a+"\n"+b);
-		for(int i=1; i<=no; i++) {
-			
-			int c=a+b;
-			      a=b;
-			      b=c;
-			      System.out.println(c);
+		int no = 10;
+		int a = 0;
+		int b = 1;
+		System.out.println(a + "\n" + b);
+		for (int i = 1; i <= no; i++) {
+
+			int c = a + b;
+			a = b;
+			b = c;
+			System.out.println(c);
 		}
-		
+
 	}
 }
