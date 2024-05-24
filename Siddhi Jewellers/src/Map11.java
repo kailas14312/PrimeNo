2d57efe92b13ac7e8e46455e4c3cdebd218dfe87
Kailas Dahiwal <mamtadahiwal@gmail.com>
2024-05-24 19:14:36 +0530

    your code done

---
+++ Siddhi Jewellers/src/Map11.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class Map11 {
+	static Scanner sc = new Scanner(System.in);
+     static int user;
+	public static void main(String[] args) {
+		System.out.println("enter your no to check prime or not");
+		for(int i=1; i<=100; i++) {
+		
+		 user = sc.nextInt();
+		getPrime(user);
+		}
+	}
+
+	public static void getPrime(int no) {
+
+		int no1 = no;
+		int counter = 0;
+
+		for (int i = 1; i <= no1; i++) {
+			if (no % i == 0) {
+				counter++;
+			}
+
+		}
+		if (counter == 2) {
+			System.out.println("prime No");
+		}else if(user==1 || user==0) {
+			System.out.println("nither prime , nor compositive");
+		}
+		else
+			System.out.println("not prime");
+
+	}
+}
